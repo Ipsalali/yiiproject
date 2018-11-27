0a4a74dbcb05852e06b8f168396e368645b1e66a
Magomedali <web-ali@yandex.ru>
2018-11-27 09:54:36 +0300

    perms

---
+++ console/import/m181121_175019_perms_for_sverka_columns.php
@@ -0,0 +1,28 @@
+<?php
+
+use console\import\Import;
+
+/**
+ * Class m181121_175019_perms_for_sverka_columns
+ */
+class m181121_175019_perms_for_sverka_columns extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m181121_175019_perms_for_sverka_columns cannot be reverted.\n";
+
+        return false;
+    }
+
+}
